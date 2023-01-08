0527da331cdd2546b2b20099e5701f94ef5e1c20
aakashdeveloper <ahanda205@gmail.com>
2023-01-08 15:44:08 +0530

    mongodb

---
+++ node/restapi/apidoc.md
@@ -0,0 +1,30 @@
+# Page 1
+
+> List of city
+> List of restaurant
+> Rest wrt to city
+> List of Meal Type
+
+
+# Page 2
+
+> Restaurants wrt to mealType
+> Restaurants wrt to mealType & Cuisine
+> Restaurants wrt to mealType & Cost
+> Sort on basis of price
+
+# Page3
+
+> Details of Restaurant
+> Menu wrt to restaurant
+
+
+# Page 4
+> Menu Details
+> Place Order
+
+# Page 5
+> List order
+> Order wrt to Email
+> Update payment details
+> Delete orders
--- node/restapi/app.js
+++ node/restapi/app.js
@@ -1,13 +1,37 @@
 let express = require('express');
 let app = express();
 let port = 6543;
+let mongo = require('mongodb');
+let MongoClient =  mongo.MongoClient;
+let mongoUrl = "mongodb://localhost:27017";
+let cors = require('cors');
+let db;
+
+app.use(cors())
 
 app.get('/',(req,res) => {
     res.send("Hii From Express")
 })
 
 app.get('/location',(req,res) => {
-    res.send("Hii From location Route")
+    db.collection('location').find().toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })                                                                                                        
 })
 
-app.listen(port)
+app.get('/restaurants',(req,res) => {
+    db.collection('restaurants').find().toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })                                                                                                        
+})
+
+//Connecting with Db
+MongoClient.connect(mongoUrl,(err,connection)=>{
+     if(err) console.log('Error while connecting')
+     db = connection.db('internfeb')
+     app.listen(port,() => {
+        console.log(`Server is running on port ${port}`)
+     })
+})
