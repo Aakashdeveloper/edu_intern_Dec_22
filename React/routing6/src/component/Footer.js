3c4b39663448d6297924bb500318e22bb7813ed5
aakashdeveloper <ahanda205@gmail.com>
2023-02-04 15:32:40 +0530

    rotuing

---
+++ React/routing6/src/component/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = ( ) => {
+    return(
+        <div>
+            <hr/>
+            <center>
+                <h3>&copy; Developer Funnel 2023</h3>
+            </center>
+            
+        </div>
+    )
+}
+
+export default Footer
